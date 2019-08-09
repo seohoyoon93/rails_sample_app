0c791015bd5de83a1b48ba98c9054121192186c6
seoho <ansndmin1@gmail.com>
2019-08-09 18:46:48 +0900

    Complete advanced setup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+# minitest gem to show colors of test results (success = green, fail = red)
+require 'minitest/reporters'
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
