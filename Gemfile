52b43dbb988eaaf272353ecacee0aa5ce1eb39d3
seoho <ansndmin1@gmail.com>
2019-08-16 21:11:47 +0900

    Implement basic login

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '~> 5.2.3'
 gem 'bootstrap-sass', '3.3.7'
 gem 'bcrypt', '3.1.12'
 
+gem 'jquery-rails'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,18 @@
-ENV['RAILS_ENV'] ||= 'test'
-require_relative '../config/environment'
-require 'rails/test_help'
+ENV["RAILS_ENV"] ||= "test"
+require_relative "../config/environment"
+require "rails/test_help"
 # minitest gem to show colors of test results (success = green, fail = red)
-require 'minitest/reporters'
+require "minitest/reporters"
 Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+
+  # Returns true if a test user is logged in.
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 
   # Add more helper methods to be used by all tests here...
   include ApplicationHelper
